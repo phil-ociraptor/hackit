00ce76f2f3c5c2e5b94b01a0bd41ec8df0ee876f
Philip Liao <philipliao@gmail.com>
2015-06-17 23:37:32 -0700

    small changes'

--- hackit.js
+++ hackit.js
@@ -8,30 +8,25 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(function(req, res) {
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
-});
-
-app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	res.type('text/plain');
-	res.status(500);
-	res.send('500 - Server Error');
-});
-
 app.listen(app.get('port'), function() {
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
 
 app.get('/', function(req, res) {
-	res.type('text/plain');
-	res.send('Hackit');
+	res.render('home');
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
 });
 
 app.use(function(req, res, next) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
-})
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
